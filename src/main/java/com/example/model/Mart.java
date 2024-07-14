7ef34291bfcd5c7045e703020cb82351be46da39
Rana Ranvijay Singh <ranaranvijaysingh9@gmail.com>
2024-07-14 14:55:43 +0530

    Created model class of MART

---
+++ src/main/java/com/example/model/Mart.java
@@ -0,0 +1,33 @@
+package com.example.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Mart {
+    @Id
+    @GeneratedValue
+    @Column(name ="id")
+    private int id;
+    @Column(name ="name")
+    private String name;
+    @Column(name ="address")
+    private Address address;
+    @Column(name ="phoneNumber")
+    private String phoneNumber;
+    @Column(name ="closeTime")
+    private String closeTime;
+    @Column(name ="openTime")
+    private String openTime;
+    @Column(name ="isOpen")
+    private boolean isOpen;
+
+}
