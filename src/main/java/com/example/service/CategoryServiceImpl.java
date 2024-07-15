ddc7726db5e4e720f0df35a2054df7511386ef4d
Rana Ranvijay Singh <ranaranvijaysingh9@gmail.com>
2024-07-16 04:15:45 +0530

    Created category controller class

--- src/main/java/com/example/service/CategoryServiceImpl.java
+++ src/main/java/com/example/service/CategoryServiceImpl.java
@@ -13,7 +13,7 @@ public class CategoryServiceImpl implements CategoryService {
     private CategoryRepo categoryRepo;
 
     @Override
-    public List<Category> getAllCategories() {
+    public List<Category> getCategories() {
         return categoryRepo.findAll();
     }
 
