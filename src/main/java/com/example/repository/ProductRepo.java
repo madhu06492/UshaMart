ea7459dab4baf5b4959d906ca6a5e43eb09ea3ee
Rana Ranvijay Singh <ranaranvijaysingh9@gmail.com>
2024-07-15 16:53:50 +0530

    Created product service and impl class and interface

--- src/main/java/com/example/repository/ProductRepo.java
+++ src/main/java/com/example/repository/ProductRepo.java
@@ -3,6 +3,9 @@ package com.example.repository;
 import com.example.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepo extends JpaRepository<Product,Long> {
 
+    List<Product> getAllProducts();
 }
---
+++ src/main/java/com/example/service/ProductImpl.java
@@ -0,0 +1,41 @@
+package com.example.service;
+
+import com.example.model.Product;
+import com.example.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductImpl implements ProductService{
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    @Override
+    public Product getProduct(long id) {
+
+        return productRepo.findById(id).get();
+    }
+
+    @Override
+    public void createProduct(Product product) {
+        productRepo.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        return productRepo.save(product);
+    }
+
+    @Override
+    public void deleteProduct(long id) {
+       Product delete= productRepo.findById(id).get();
+        productRepo.delete(delete);
+    }
+}
+
