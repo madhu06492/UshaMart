5f1fe7827251a61182bc2b52666b18fae3deb140
Rana Ranvijay Singh <ranaranvijaysingh9@gmail.com>
2024-07-15 17:10:21 +0530

    Formated code

--- src/main/java/com/example/repository/ProductRepo.java
+++ src/main/java/com/example/repository/ProductRepo.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductRepo extends JpaRepository<Product,Long> {
+public interface ProductRepo extends JpaRepository<Product, Long> {
 
     List<Product> getAllProducts();
-}
