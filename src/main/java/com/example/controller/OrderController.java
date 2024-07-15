da983bdd4d5c7267f6f57688797ba41f2f20e09b
Rana Ranvijay Singh <ranaranvijaysingh9@gmail.com>
2024-07-16 03:44:07 +0530

    commiting changers

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -15,27 +15,27 @@ public class OrderController {
     private OrderService orderService;
 
     @GetMapping("/order")
-    public List<Order> getOrders(){
+    public List<Order> getOrders() {
         return this.orderService.getOrders();
     }
 
     @GetMapping("/order/{orderId}")
-    public Order getOrder(@PathVariable long orderId){
-      return this.orderService.getOrder(orderId);
+    public Order getOrder(@PathVariable long orderId) {
+        return this.orderService.getOrder(orderId);
     }
 
     @PostMapping("/order")
-    public void createOrder(@RequestBody Order order){
-         this.orderService.createOrder(order);
+    public void createOrder(@RequestBody Order order) {
+        this.orderService.createOrder(order);
     }
 
     @PutMapping("/order")
-    public void updateOrder(@RequestBody Order order){
+    public void updateOrder(@RequestBody Order order) {
         this.orderService.updateOrder(order);
     }
 
     @DeleteMapping("/order/{orderId}")
-    public void deleteOrder(@PathVariable long  orderId){
+    public void deleteOrder(@PathVariable long orderId) {
         this.orderService.deleteOrder(orderId);
     }
 
