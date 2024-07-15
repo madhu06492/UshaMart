71a129fad1f1a1b9197a2f663182dfa88732ee2a
Admin <admin@Admins-MacBook-Pro.local>
2024-07-15 17:37:05 +0530

    Created Service of Address

---
+++ src/main/java/com/example/service/AddressServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.service;
+
+import com.example.model.Address;
+import com.example.repository.AddressRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    private AddressRepo addressRepo;
+
+    @Override
+    public List<Address> getAddresses() {
+        return addressRepo.findAll();
+    }
+
+    @Override
+    public Address getAddress(long id) {
+        return addressRepo.findById(id).get();
+    }
+
+    @Override
+    public void createAddress(Address address) {
+        addressRepo.save(address);
+    }
+
+    @Override
+    public void updateeAddress(Address address) {
+        addressRepo.save(address);
+    }
+
+    @Override
+    public void deleteAddress(long id) {
+        Address addressDelete = addressRepo.findById(id).get();
+        addressRepo.delete(addressDelete);
+    }
+}
